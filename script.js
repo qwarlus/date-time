a391c15c3351abde814995b9c68130aa5771045e
qwarlus <qwarlus.osnova@gmail.com>
2024-09-02 22:23:09 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const [dateEl, monthEl, yearEl, hourEl, minEl, secEl, dayEl] = document.querySelectorAll("b")
+const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+updateTime()
+setInterval(updateTime, 1000)
+
+function updateTime() {
+    const { date, month, year, hours, minutes, seconds, day } = getData()
+
+    dateEl.innerText = date
+    monthEl.innerText = month
+    yearEl.innerText = year
+    hourEl.innerText = hours
+    minEl.innerText = minutes
+    secEl.innerText = seconds
+    dayEl.innerText = day
+}
+
+function getData() {
+    const d = new Date
+
+    return {
+        date: d.getDate().toString().padStart(2, 0),
+        month: (d.getMonth() + 1).toString().padStart(2, 0),
+        year: (d.getYear() - 100).toString().padStart(2, 0),
+        hours: d.getHours().toString().padStart(2, 0),
+        minutes: d.getMinutes().toString().padStart(2, 0),
+        seconds: d.getSeconds().toString().padStart(2, 0),
+        day: days[d.getDay()],
+    }
+}
